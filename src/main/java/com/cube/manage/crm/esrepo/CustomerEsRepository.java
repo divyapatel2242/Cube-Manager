3df24514bda82da4cebe7bcddde2dc100d94b311
divyapatel2244 <divyapatel2241@gmail.com>
2025-01-19 15:49:30 +0530

    [Disable-Elastic-Search] Disabled a ES

--- src/main/java/com/cube/manage/crm/esrepo/CustomerEsRepository.java
+++ src/main/java/com/cube/manage/crm/esrepo/CustomerEsRepository.java
@@ -1,9 +1,9 @@
-package com.cube.manage.crm.esrepo;
-
-import com.cube.manage.crm.esdocument.CustomerEs;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerEsRepository extends ElasticsearchRepository<CustomerEs,Integer> {
-}
+//package com.cube.manage.crm.esrepo;
+//
+//import com.cube.manage.crm.esdocument.CustomerEs;
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface CustomerEsRepository extends ElasticsearchRepository<CustomerEs,Integer> {
+//}
