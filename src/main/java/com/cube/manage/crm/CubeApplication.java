3cf3b9fed0d704b122bb8cebd538e344e4d55db5
divyapatel2244 <divyapatel2241@gmail.com>
2025-02-16 17:34:45 +0530

    Revert "[main] changed config"
    This reverts commit 6a601919f274eaa5257fbdc2f84033fee3343e1a.

--- src/main/java/com/cube/manage/crm/CubeApplication.java
+++ src/main/java/com/cube/manage/crm/CubeApplication.java
@@ -2,7 +2,6 @@ package com.cube.manage.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -13,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Divya Patel
  * */
 
-@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+@SpringBootApplication
 @EnableAsync
 public class CubeApplication {
 
