c19597d66d1cd9ee40f2160b23280f9e1593f115
Divya patel <166705591+divyapatel2242@users.noreply.github.com>
2025-02-16 17:35:21 +0530

    Revert "[Disable-Elastic-Search] Disabled a ES"

--- src/main/java/com/cube/manage/crm/esrepo/ProductEsRepository.java
+++ src/main/java/com/cube/manage/crm/esrepo/ProductEsRepository.java
@@ -1,9 +1,9 @@
-//package com.cube.manage.crm.esrepo;
-//
-//import com.cube.manage.crm.esdocument.ProductEs;
-//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface ProductEsRepository extends ElasticsearchRepository<ProductEs,String> {
-//}
+package com.cube.manage.crm.esrepo;
+
+import com.cube.manage.crm.esdocument.ProductEs;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductEsRepository extends ElasticsearchRepository<ProductEs,String> {
+}
